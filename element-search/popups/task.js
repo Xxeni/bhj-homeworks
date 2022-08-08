545fbc19126b6a363d975d31f8a4c7d25dd5aa30
Ксения <ksenia2833@gmail.com>
2022-08-09 01:09:06 +0400

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,27 +1,21 @@
-const openModal = document.querySelectorAll('div.modal');
-const closeModal = document.querySelectorAll('div.modal__close')
-const modal = document.getElementById('modal_main');
-const objModalSuccess = document.getElementsByClassName('show-success')
-const modalSuccess= document.getElementById('modal_success');
+let active = document.getElementById("modal_main");
+active.className += " modal_active";
 
-document.body.addEventListener("click", handleModalActiv);
+let closeBth = document.getElementsByClassName("modal__close");
 
-	for(let i = 0; i < closeModal.length; i++) {
-	closeModal[i].addEventListener("click", handleOpenModal);
-	}
+let bthSuccess = document.querySelector(".show-success");
+console.log(bthSuccess)
+bthSuccess.addEventListener("click", () =>  {
+    document.getElementById("modal_success").className += " modal_active";
 
-	objModalSuccess[0].addEventListener("click", handleModalSuccess);
+})
 
-function handleOpenModal() {
-for(let i = 0; i < openModal.length; i++) {
-    	openModal[i].style.display = 'none';
-		}   
-    }
-
-    function handleModalSuccess() {
-        modalSuccess.style.display = 'flex';
-        }
-
-function handleModalActiv() {
-    	modal.classList.add('modal_active');
+for (let item of closeBth) {
+item.onclick = () => {
+    if (item.parentElement.parentElement.classList.contains("modal_active") && item.classList.contains("modal__close_times")) {
+        item.parentElement.parentElement.classList.remove("modal_active");
+        active.classList.remove("modal_active");
+    } 
+    
 }
+} 
